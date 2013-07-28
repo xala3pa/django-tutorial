ea0196fadc802247a10ddeb63c16c30978a4bdb2
xala3pa <salazar3pa@gmail.com>
2013-07-28 17:58:39 +0200

    Code for automatically generated Admin site

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,7 +12,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/xala3pa/workarea/django-tutorial/mysite/db/sqlite3.db',                      # Or path to database file if using sqlite3.
+        #'NAME': '/Users/xala3pa/workarea/django-tutorial/mysite/db/sqlite3.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/xala/workarea/django-tutorial/mysite/db/sqlite3.db',  #Ubuntu
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -111,6 +112,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/home/xala/workarea/django-tutorial/mysite/templates',
+
 )
 
 INSTALLED_APPS = (
@@ -121,9 +124,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'polls',
 )
 
 # A sample logging configuration. The only tangible logging
