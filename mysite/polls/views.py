fb3373deb2aa5678c6f0c30627df3dc2cf90da53
xala3pa <salazar3pa@gmail.com>
2013-07-31 23:58:20 +0200

    more on views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,13 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render	
+
+from polls.models import Poll
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
     return HttpResponse("You're looking at poll %s." % poll_id)
