af23582fe352262609d4bd34436fe35ec75426aa
xala3pa <salazar3pa@gmail.com>
2013-08-01 23:41:36 +0200

    Using a shortcut: get_object_or_404() to render in a view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.http import HttpResponse ,Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from polls.models import Poll
 
@@ -10,10 +10,7 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    try:
-        poll = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
+    poll = get_object_or_404(Poll, pk=poll_id)
     return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
