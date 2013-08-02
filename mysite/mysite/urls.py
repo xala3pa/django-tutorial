e38ca47f1f43dbdc3ddd5f40e12763ae942a4af3
xala3pa <salazar3pa@gmail.com>
2013-08-02 18:06:26 +0200

    NAmespacing URL names

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,7 +13,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 
 )
